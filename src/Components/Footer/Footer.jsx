529967aef5974ea8026a402aff743a1c7c537aa2
MichealWong <wangyan.im@gmail.com>
2022-11-23 19:31:44 +0800

    Footer component

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,10 +1,160 @@
-import React from 'react'
-import "./Footer.css"
+import React from "react";
+import "./Footer.css";
+import { FiChevronRight, FiSend } from "react-icons/fi";
+import { MdOutlineTravelExplore } from "react-icons/md";
+
+import { AiOutlineTwitter } from "react-icons/ai";
+import { AiFillYoutube } from "react-icons/ai";
+import { FaTripadvisor } from "react-icons/fa";
+import { AiFillInstagram } from "react-icons/ai";
+
+import video from "../../Assets/cartoon.mp4";
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+    return (
+        <section className="footer">
+            <div className="videoDiv">
+                <video src={video} autoPlay muted loop type="video/mp4"></video>
+            </div>
 
-export default Footer
+            <div className="secContent container">
+                <div className="contactDiv flex">
+                    <div className="text">
+                        <small>KEEP IN TOUCH</small>
+                        <h2>Travel with us</h2>
+                    </div>
+
+                    <div className="inputDiv flex">
+                        <input type="text" placeholder="Enter Email Address" />
+                        <button className="btn flex" type="submit">
+                            SEND <FiSend className="icon" />
+                        </button>
+                    </div>
+                </div>
+
+                <div className="footerCard flex">
+                    <div className="footerIntro flex">
+                        <div className="logoDiv">
+                            <a href="#" className="logo flex">
+                                <MdOutlineTravelExplore className="icon" />{" "}
+                                Travel.
+                            </a>
+                        </div>
+
+                        <div className="footerParagraph">
+                            ‘It’s not safe and it’s not right.’ Qatar says all
+                            are welcome to the World Cup but some LGBTQ soccer
+                            fans are staying away
+                        </div>
+
+                        <div className="footerSocials flex">
+                            <AiOutlineTwitter className="icon" />
+                            <AiFillYoutube className="icon" />
+                            <AiFillInstagram className="icon" />
+                            <FaTripadvisor className="icon" />
+                        </div>
+                    </div>
+
+                    <div className="footerLinks grid">
+                        {/*group one*/}
+                        <div className="linkGroup">
+                            <span className="groupTitle">OUR AGENCY</span>
+
+                            <li className="footerList flex">
+                                <FiChevronRight className="icon" />
+                                Services
+                            </li>
+
+                            <li className="footerList flex">
+                                <FiChevronRight className="icon" />
+                                Insurance
+                            </li>
+
+                            <li className="footerList flex">
+                                <FiChevronRight className="icon" />
+                                Agency
+                            </li>
+
+                            <li className="footerList flex">
+                                <FiChevronRight className="icon" />
+                                Tourism
+                            </li>
+
+                            <li className="footerList flex">
+                                <FiChevronRight className="icon" />
+                                Payment
+                            </li>
+                        </div>
+
+                        {/*group two*/}
+                        <div className="linkGroup">
+                            <span className="groupTitle">PARTNERS</span>
+
+                            <li className="footerList flex">
+                                <FiChevronRight className="icon" />
+                                Services
+                            </li>
+
+                            <li className="footerList flex">
+                                <FiChevronRight className="icon" />
+                                Insurance
+                            </li>
+
+                            <li className="footerList flex">
+                                <FiChevronRight className="icon" />
+                                Agency
+                            </li>
+
+                            <li className="footerList flex">
+                                <FiChevronRight className="icon" />
+                                Tourism
+                            </li>
+
+                            <li className="footerList flex">
+                                <FiChevronRight className="icon" />
+                                Payment
+                            </li>
+                        </div>
+
+                        {/*group three*/}
+                        <div className="linkGroup">
+                            <span className="groupTitle">LAST MINUTE</span>
+
+                            <li className="footerList flex">
+                                <FiChevronRight className="icon" />
+                                Services
+                            </li>
+
+                            <li className="footerList flex">
+                                <FiChevronRight className="icon" />
+                                Insurance
+                            </li>
+
+                            <li className="footerList flex">
+                                <FiChevronRight className="icon" />
+                                Agency
+                            </li>
+
+                            <li className="footerList flex">
+                                <FiChevronRight className="icon" />
+                                Tourism
+                            </li>
+
+                            <li className="footerList flex">
+                                <FiChevronRight className="icon" />
+                                Payment
+                            </li>
+                        </div>
+                    </div>
+
+                    <div className="footerDiv flex">
+                      <small>BEST TRAVEL WEBSITE THEME</small>
+                      <small>COPYRIGHTS RESERVED - ISRATECH 2022</small>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Footer;
